b070dc244e0eca07c3f022ecb7a1c1fa89377080
rg3915 <regis42santos@gmail.com>
2023-07-06 02:57:01 -0300

    Adiciona csrf=True na API

--- backend/api.py
+++ backend/api.py
@@ -3,7 +3,7 @@ from ninja import NinjaAPI
 from backend.crm.crm_api import router as crm_router
 from backend.servico.servico_api import router as servico_router
 
-api = NinjaAPI()
+api = NinjaAPI(csrf=True)
 
 api.add_router('/crm/', crm_router)
 api.add_router('/servico/', servico_router)
