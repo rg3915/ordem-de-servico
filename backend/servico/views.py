8c409e6732c9b4a0f7511d16554bdfa10a62eb53
rg3915 <regis42santos@gmail.com>
2023-07-05 21:31:54 -0300

    Retorna lista de Ordens de serviço e os detalhes de cada um.

--- backend/servico/views.py
+++ backend/servico/views.py
@@ -1,13 +1,12 @@
 from django.shortcuts import render
+from .models import OrdemServico
 
 
 def ordem_servico_list(request):
     template_name = 'servico/ordem_servico_list.html'
-    # object_list = OrdemServico.objects.all()
-    # context = {
-    #     'object_list': object_list
-    # }
-    return render(request, template_name)
+    object_list = OrdemServico.objects.all()
+    context = {'object_list': object_list}
+    return render(request, template_name, context)
 
 
 def ordem_servico_create(request):
@@ -15,7 +14,10 @@ def ordem_servico_create(request):
 
 
 def ordem_servico_detail(request, pk):
-    ...
+    template_name = 'servico/ordem_servico_detail.html'
+    instance = OrdemServico.objects.get(pk=pk)
+    context = {'object': instance}
+    return render(request, template_name, context)
 
 
 def ordem_servico_update(request, pk):
